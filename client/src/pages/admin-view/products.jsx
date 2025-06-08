7a3f9405f385b0685fe26708710c3729c75e68ea
wanjaiya <kangethewanjaiya@gmail.com>
2025-06-08 16:31:53 +0300

    product start

--- client/src/pages/admin-view/products.jsx
+++ client/src/pages/admin-view/products.jsx
@@ -1,5 +1,15 @@
+import { Button } from "@/components/ui/button";
+import { Fragment } from "react";
+
 function AdminProducts() {
-  return <div>Admin Products</div>;
+  return (
+    <Fragment>
+      <div className="mb-5 w-full flex justify-end">
+        <Button>Add New Product</Button>
+      </div>
+      <div className="grid gap-4 md:grid-cols-3 lg:grid-cols-4"></div>
+    </Fragment>
+  );
 }
 
 export default AdminProducts;
